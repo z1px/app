81ad133c2c3ba2a7f474ded9da16006e34961016
kevin <936957321@qq.com>
2020-01-20 14:38:13 +0800

    优化

--- src/Http/Services/Admins/PermissionsService.php
+++ src/Http/Services/Admins/PermissionsService.php
@@ -7,13 +7,14 @@ use Z1px\App\Models\Admins\PermissionsModel;
 use Z1px\App\Traits\Eloquent\ToAdd;
 use Z1px\App\Traits\Eloquent\ToDelete;
 use Z1px\App\Traits\Eloquent\ToInfo;
+use Z1px\App\Traits\Eloquent\ToList;
 use Z1px\App\Traits\Eloquent\ToListAll;
 use Z1px\App\Traits\Eloquent\ToUpdate;
 
 class PermissionsService extends PermissionsModel
 {
 
-    use ToInfo, ToAdd, ToUpdate, ToDelete, ToListAll;
+    use ToList, ToInfo, ToAdd, ToUpdate, ToDelete, ToListAll;
 
     /**
      * 权限列表
